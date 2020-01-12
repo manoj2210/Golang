1e07789cd4d3b76303d6357ecb1741e47ce171ff
Manoj2210 <manojkumarmaths1000@gmail.com>
2020-01-12 12:47:41 +0530

    Into to MongoDB

---
+++ Rest API/src/MongoDB/Basics/main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"fmt"
+	"gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
+	"log"
+)
+
+type Movie struct {
+	Name      string   `bson:"name"`
+	Year      string   `bson:"year"`
+	Directors []string `bson:"directors"`
+	Writers   []string `bson:"write rs"`
+	BoxOffice `bson:"boxOffice"`
+}
+
+type BoxOffice struct {
+	Budget uint64 `bson:"budget"`
+	Gross  uint64 `bson:"gross"`
+}
+
+func main() {
+	session, err := mgo.Dial("127.0.0.1")
+	if err != nil {
+		panic(err)
+	}
+	defer session.Close()
+	c := session.DB("appdb").C("movies")
+	// Create a movie
+	darkNight := &Movie{
+		Name:      "The Dark Knight",
+		Year:      "2008",
+		Directors: []string{"Christopher Nolan"},
+		Writers:   []string{"Jonathan Nolan", "Christopher Nolan"},
+		BoxOffice: BoxOffice{
+			Budget: 185000000,
+			Gross:  533316061,
+		},
+	}
+	// Insert into MongoDB
+	err = c.Insert(darkNight)
+	if err != nil {
+		log.Fatal(err)
+	}
+	// Now query the movie back
+	result := Movie{}
+	// bson.M is used for nested fields
+	err = c.Find(bson.M{"boxOffice.budget": bson.M{"$gt": 150000000}}).One(&result)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Movie:", result.Name)
+}
